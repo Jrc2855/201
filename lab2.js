eda648bb3e11b4c73ec5efd74e3f6da44fab950e
John Chavez <jrc2855@gmail.com>
2021-09-14 20:50:21 -0400

    Add files for lab 2

---
+++ lab2.js
@@ -0,0 +1,24 @@
+function firstQuestion(){
+    let firstOne = prompt("Do you know my name?");
+    console.log(firstOne);
+}
+
+function secondQuestion(){
+    let secondOne = prompt("Did you know that I have three kids?");
+    console.log(secondOne);
+}
+
+function thirdQuestion(){
+    let thirdOne = prompt("Did you know that I am currently living in Florida?");
+    console.log(thirdOne);
+}
+
+function fourthQuestion(){
+let fourthOne = prompt("Is my favorite drink coffee?");
+console.log(fourthOne);
+}
+
+function fifthQuestion(){
+let fifthOne = prompt("Do you like my page?");
+console.log(fifthOne);
+}
---
+++ scrapApp.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+if (age == 31){
+    console.log('!Nice!');
+} else if (age < 21){
+    console.log('Are you old enough to be here');
+}
